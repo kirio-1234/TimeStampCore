ba38dbad7e55c998582e1c6b75b7d625b3c91070
Iori Suzuki <ioribear@gmail.com>
2024-01-10 00:36:13 +0900

    Viewを作成

--- TimeStampCore/Sources/TimeStampCore/TimeStampCore.swift
+++ TimeStampCore/Sources/TimeStampCore/TimeStampCore.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct TimeStampCore: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello, world!")
+    }
+}
