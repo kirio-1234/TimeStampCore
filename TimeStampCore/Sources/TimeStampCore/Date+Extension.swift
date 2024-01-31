e3327ebfbfd74780165e023b4f8a7315ae4de00d
Iori Suzuki <ioribear@gmail.com>
2024-01-31 22:54:20 +0900

    DateのExtensionプロパティを追加

---
+++ TimeStampCore/Sources/TimeStampCore/Date+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Date+Extension.swift
+//
+//
+//  Created by Iori Suzuki on 2024/01/31.
+//
+import Foundation
+
+public extension Date {
+    var yyyyMMDDEEEHHmm: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = .init(identifier: .gregorian)
+        dateFormatter.locale = .init(identifier: "ja_JP")
+        dateFormatter.dateFormat = "yy/MM/DD (EEE) HH:mm"
+        return dateFormatter.string(from: self)
+    }
+}
